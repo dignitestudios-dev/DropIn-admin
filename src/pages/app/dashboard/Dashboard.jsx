25fb661acd1ea9ebbdd4821e504da9e67b456532
zackcole <zeeshan.ahmed@launchbox.pk>
2025-03-14 11:26:22 +0500

    update:graph file name fix

--- src/pages/app/dashboard/Dashboard.jsx
+++ src/pages/app/dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ import {
   LogInBg,
 } from "../../../assets/export";
 import HorizontalBarChart from "../../../components/app/dashboard/BarChart";
-import { LineGraph } from "../../../components/app/dashboard/Graph";
+import { LineGraph } from "../../../components/app/dashboard/graph";
 import { useUsers } from "../../../hooks/api/Get";
 const Dashboard = () => {
   const { data, loading, pagination } = useUsers("/admin/users", 1);
